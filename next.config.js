cbc06c42d3b66c815c4ddd29efde4775e500df07
kauanbrtech <kauandasilva@brtech.dev>
2025-12-01 16:01:48 -0300

    novo deploy

--- next.config.js
+++ next.config.js
@@ -2,18 +2,17 @@ const { parsed: localEnv } = require('dotenv').config();
 const webpack = require('webpack');
 
 module.exports = {
-    webpack: (config) => {
-      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-      config.module.rules.push({
-        test: /\.(woff2?|ttf|eot|svg|png|jpe?g|gif|pdf|node)$/,
-        use: [
-          {
-            loader: 'raw-loader',
-          },
-        ],
-      });
-  
-      return config;
-    },
-  };
-  
+  webpack: (config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    config.module.rules.push({
+      test: /\.(woff2?|ttf|eot|svg|png|jpe?g|gif|pdf)$/, 
+      use: [
+        {
+          loader: 'raw-loader',
+        },
+      ],
+    });
+
+    return config;
+  },
+};
